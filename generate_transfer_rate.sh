ebf00ba9f1eb550a75e3a135b10cbd2792268139
Nicolas Beguier <nicolas.beguier@schibsted.com>
2018-11-08 08:03:27 +0100

    Clean and improve generate_transfer_rate.sh by just pasting the curl

--- generate_transfer_rate.sh
+++ generate_transfer_rate.sh
@@ -2,5 +2,11 @@
 
 TRANSFER_BATCH=$1
 
-for player in $(bash $1 | jq -c  '.[] | .player | {name, value, price, position}' 2>/dev/null | sed 's/ /_/g'); do NAME=$(echo $player | awk -F'"' '{print $4}'); VALUE=$(echo $player | awk -F'"' '{print $7}' | sed 's/://g' | sed 's/,//g'); PRICE=$(echo $player | awk -F'"' '{print $9}' | sed 's/://g' | sed 's/,//g'); POSTITION=$(echo $player | awk -F'"' '{print $11}'); echo "$NAME ($POSTITION) : $((PRICE*100/VALUE))"; done | sort -k4 -n
-
+for player in $(bash "${TRANSFER_BATCH}" 2>/dev/null | grep '"id"' | jq -c  '.[] | .player | {name, value, price, position}' 2>/dev/null | sed 's/ /_/g')
+do
+    NAME=$(echo "${player}" | awk -F'"' '{print $4}')
+    VALUE=$(echo "${player}" | awk -F'"' '{print $7}' | sed 's/://g' | sed 's/,//g')
+    PRICE=$(echo "${player}" | awk -F'"' '{print $9}' | sed 's/://g' | sed 's/,//g')
+    POSTITION=$(echo "${player}" | awk -F'"' '{print $11}' | sed 's/:1/A/g' | sed 's/:2/M/g'| sed 's/:3/D/g' | sed 's/:4/G/g')
+    echo "$NAME ($POSTITION) : $((PRICE*100/VALUE))"
+done | sort -k4 -n -r
